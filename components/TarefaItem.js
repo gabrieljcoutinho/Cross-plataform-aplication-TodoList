c2f3f30a87fe6993ccf5c1a4b6e226ab7301d51b
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-04-02 10:53:48 -0300

    separando arquivos de conteudo e css

--- components/TarefaItem.js
+++ components/TarefaItem.js
@@ -3,10 +3,10 @@ import {
   View,
   Text,
   TouchableOpacity,
-  StyleSheet,
   Animated,
   Switch
 } from 'react-native';
+import styles from '../Css/styleTarefa';
 
 export default function TarefaItem({ tarefa, onRemover, onAlternar }) {
   const scale = useRef(new Animated.Value(1)).current;
@@ -47,30 +47,3 @@ export default function TarefaItem({ tarefa, onRemover, onAlternar }) {
     </Animated.View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#1c1c1c',
-    padding: 15,
-    borderRadius: 12,
-    marginBottom: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  },
-  concluido: {
-    backgroundColor: '#0f2b1f'
-  },
-  texto: {
-    flex: 1,
-    marginHorizontal: 10,
-    color: '#fff'
-  },
-  textoConcluido: {
-    textDecorationLine: 'line-through',
-    color: '#00ff88'
-  },
-  delete: {
-    fontSize: 18
-  }
-});
